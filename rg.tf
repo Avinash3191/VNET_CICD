945a3fa1652f476b270b28449c30e69a7c79d79e
Avinash3191 <avinash.miet2008@gmail.com>
2025-07-24 22:48:29 +0530

    Updaterg1.tf

--- rg.tf
+++ rg.tf
@@ -1,17 +1,21 @@
 terraform {
+  required_version = ">= 1.1.0"
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-       version = "4.37.0"
+      source  = "hashicorp/azurerm"
+      version = "4.37.0"
     }
   }
 }
 
 provider "azurerm" {
-   features {}
+  features {}
   subscription_id = "def6c89e-a855-4b2a-a2d3-a1bc15337302"
 }
+
 resource "azurerm_resource_group" "rglocal" {
-  name     = "rg for CICD"
+  name     = "rg-for-CICD"  # ✅ No spaces
   location = "West US"
 }
+
